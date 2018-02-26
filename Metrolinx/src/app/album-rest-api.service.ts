5371bf9ed116fba64927100ce8da53afb40520ed
mduberstein <mduberstein@gmail.fake>
2018-02-25 22:14:30 -0500

    JSONP attempt commented out

--- Metrolinx/src/app/album-rest-api.service.ts
+++ Metrolinx/src/app/album-rest-api.service.ts
@@ -3,12 +3,14 @@ import {Observable} from 'rxjs/RX'
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch'
 
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Jsonp} from '@angular/http'
+import {Http} from '@angular/http'
+import {Response, Headers, RequestOptions} from '@angular/http';
 
 @Injectable()
 export class AlbumRestApiService {
 
-  constructor(private http:Http) {}
+  constructor(private http:Http, private jsonp:Jsonp ) {}
 
   getLocation(selectedCity:string): Observable<any[]>{
       let url = `https://www.metaweather.com/api/location/search/?query=${selectedCity}`;
@@ -23,6 +25,22 @@ export class AlbumRestApiService {
     //     return response.json();
     //   }).catch(this.handleError);
   }
+
+//   getLocation(selectedCity:string): Observable<any[]>{
+//     let url = `https://www.metaweather.com/api/location/search/?query=${selectedCity}&callback=JSON_CALLBACK`;
+//     let headers = new Headers({'Accept':'application/javascript'} );
+//     let options = new RequestOptions ({headers: headers});
+//     let observable1 = this.jsonp.request(url, options);
+//     var observable2 = observable1.map((response:Response)=>{
+//       return response.json();
+//     });
+//     var observable3 = observable2.catch(this.handleError);
+//     return observable3;
+//   // return this.http.get(`https://www.metaweather.com/api/location/search/?query=${selectedCity}`)
+//   //   .map((response:Response)=> {
+//   //     return response.json();
+//   //   }).catch(this.handleError);
+// }
 
   getWeatherAndSunRiseSetData(selectedCityWoeid:number):Observable<any>{
     return this.http.get(`https://www.metaweather.com/api/location/${selectedCityWoeid}`).map((response:Response)=>{return response.json();}).catch(this.handleError);
--- Metrolinx/src/app/app.module.ts
+++ Metrolinx/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import {HttpModule} from '@angular/http'
+import {JsonpModule} from '@angular/http'
 
 import { AppComponent } from './app.component';
 import { AlbumListComponent } from './album-list/album-list.component';
@@ -16,7 +17,8 @@ import {AlbumRestApiService} from './album-rest-api.service'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule
   ],
   providers: [AlbumRestApiService],
   // bootstrap: [AppComponent]
